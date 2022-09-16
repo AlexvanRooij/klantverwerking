66d9a32da6fbc022994a7d580bdeb6922f69a434
AlexvanRooij <alex.rooij@icloud.com>
2022-09-16 12:31:51 +0200

    Final

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require_once('vendor/autoload.php');
+
+if(!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["oms"])) {
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'alexvanrooij.nl';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'contact@alexvanrooij.nl';
+        $mail->Password   = '0409-DerpAlex!';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
+
+        $mail->addAddress($_POST["email"]);
+        $mail->setFrom('contact@alexvanrooij.nl', 'Contact');
+        $mail->addCC('alex.rooij@icloud.com');
+
+        $mail->isHTML(true);
+        $mail->Subject = $_POST["oms"];
+        $mail->Body    = 'temp';
+        $mail->AltBody = 'temp';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
+
+<form method="POST" action="">
+    <input type="text" name="name">
+    <input type="email" name="email">
+    <input type="text" name="oms">
+    <button type="submit">
+</form>
